3447c92ae74b84e6d16f1ca98ab8b8d7bf372929
Ira Zubrytska <iryna.zubrytska@ukma.edu.ua>
2021-05-11 20:19:49 +0300

    Initial commit

---
+++ src/main/java/com/iraz/App.java
@@ -0,0 +1,13 @@
+package com.iraz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
