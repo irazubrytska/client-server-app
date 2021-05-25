7433b20ba96c75f22d02fe0b9bf740cac18e72af
Ira Zubrytska <iryna.zubrytska@ukma.edu.ua>
2021-05-25 13:15:09 +0300

    Final version

--- src/test/java/com/iraz/AppTest.java
+++ src/test/java/com/iraz/AppTest.java
@@ -1,20 +1,66 @@
 package com.iraz;
 
-import static org.junit.Assert.assertTrue;
-
+import  com.google.common.primitives.UnsignedLong;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import javax.crypto.*;
+import java.security.*;
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void createPacket(){
+        byte b=0;
+        UnsignedLong packetId=UnsignedLong.valueOf(10L);
+        String str="this is some message";
+        int cType=10;
+        int userId=2;
+        Message message=new Message(cType,userId,str);
+        Packet pack=new Packet(b,packetId,message);
     }
+
+    @Test
+    public void encryptDecryptTest() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Cryptor cryptor=new Cryptor();
+        String original = "This is the message I want To Encrypt.";
+        byte[] encrypted = cryptor.encrypt(original);
+        String decrypted = cryptor.decrypt(encrypted);
+        Assert.assertEquals(original,decrypted);
+//        System.out.println("original: "+original);
+//        System.out.println("encrypted: "+ DatatypeConverter.printHexBinary(encrypted));
+//        System.out.println("decrypted: "+decrypted);
+    }
+
+    @Test
+    public void packetTest() throws Exception {
+        //same packages
+        String test_message="this is some test message";
+        Packet pack1 = new Packet((byte) 6, UnsignedLong.valueOf(1L), new Message(5, 4, test_message));
+        Packet pack2 = new Packet((byte) 6, UnsignedLong.valueOf(1L), new Message(5, 4, test_message));
+
+        Cryptor cryptor=new Cryptor();
+
+        //encode both
+        byte[] pack1Bytes=pack1.toPacket(cryptor);
+        byte[] pack2Bytes=pack2.toPacket(cryptor);
+
+        //decode both
+        pack1=new Packet(pack1Bytes,cryptor);
+        pack2=new Packet(pack2Bytes,cryptor);
+
+        Assert.assertEquals(pack1.getBSrc(), pack2.getBSrc());
+        Assert.assertEquals(pack1.getBPktId(), pack2.getBPktId());
+        Assert.assertEquals(pack1.getWLen(), pack2.getWLen());
+
+        Assert.assertEquals(pack1.getWCrc16_1(), pack2.getWCrc16_1());
+        Assert.assertEquals(pack1.getWCrc16_2(), pack2.getWCrc16_2());
+
+        Assert.assertEquals(pack1.getBMsg().getCType(), pack2.getBMsg().getCType());
+        Assert.assertEquals(pack1.getBMsg().getBUserId(), pack2.getBMsg().getBUserId());
+        Assert.assertArrayEquals(pack1.getBMsg().getMessage(), pack2.getBMsg().getMessage());
+        Assert.assertEquals(test_message, new String(pack2.getBMsg().getMessage()));
+    }
+
 }
